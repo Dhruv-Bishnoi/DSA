ed874dff440754f9a61b077f8a58dc91189ecf8e
dhruv bishnoi <bishnoishab502@gmail.com>
2026-02-24 00:19:33 +0530

    Create stocksell$buy.java

---
+++ stocksell$buy.java
@@ -0,0 +1,25 @@
+public class stocksell$buy {
+    public static void main(String[] args) {
+        int prices[] ={7,1,5,3,6,4};
+        int buy_price = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buy_price<prices[i]) {
+
+                int profit = prices[i]-buy_price;
+                maxprofit=Math.max(maxprofit, profit);
+
+            }    
+            else{
+                buy_price=prices[i];
+
+            }
+        }
+        System.out.println(maxprofit);
+
+
+
+    }
+    
+}
