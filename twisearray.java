17821c103bb16794b6e0d1c4a7142218d18cd0d8
dhruv bishnoi <bishnoishab502@gmail.com>
2026-02-24 14:21:08 +0530

    Update twisearray.java

--- twisearray.java
+++ twisearray.java
@@ -1,19 +1,43 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class twisearray {
     public static void main(String[] args) {
-        int nums[] ={1,2,3,4,5,6,8};
+        int nums[] ={1,2,3,4,5,6,8,8};
 
-        for (int i = 0; i < nums.length; i++) {
-            for(int j = i+1;j<nums.length;j++){
-                if (nums[i]==nums[j]) {
-                    System.out.println(true);
-                    return;
-                    
-                }
-               
+
+        //using sorting
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i]==nums[i-1]) {
+                System.out.println(true);
+                return;
+                
             }
-            
+
             
         }
         System.out.println(false);
+
+        //using brute force
+        // for (int i = 0; i < nums.length; i++) {
+        //     for(int j = i+1;j<nums.length;j++){
+        //         if (nums[i]==nums[j]) {
+        //             System.out.println(true);
+        //             return;
+                    
+        //         }
+               
+        //     }
+            
+            
+        // }
+        // System.out.println(false);
+
+
+
+
     }
 }
