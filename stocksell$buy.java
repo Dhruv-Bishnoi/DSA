c8d29d2d3ea789745ee5de0bb11e21316b73bd96
dhruv bishnoi <bishnoishab502@gmail.com>
2026-02-24 14:15:17 +0530

    .

--- stocksell$buy.java
+++ stocksell$buy.java
@@ -1,24 +1,25 @@
 public class stocksell$buy {
     public static void main(String[] args) {
         int prices[] ={7,1,5,3,6,4};
-        int buy_price = Integer.MAX_VALUE;
-        int maxprofit = 0;
+       int buy_price =prices[0];
+       int Maxprofit =0; 
 
-        for (int i = 0; i < prices.length; i++) {
-            if (buy_price<prices[i]) {
-
-                int profit = prices[i]-buy_price;
-                maxprofit=Math.max(maxprofit, profit);
-
-            }    
-            else{
-                buy_price=prices[i];
-
-            }
+       for(int i = 1; i<prices.length;i++){
+        int selling_price = prices[i];
+        if (buy_price<selling_price) {
+             int profit =(selling_price-buy_price);
+            Maxprofit=Math.max(profit, Maxprofit);
+            
         }
-        System.out.println(maxprofit);
+        else{
+            buy_price=selling_price;
+        }
 
 
+        
+        
+       }
+       System.out.println(Maxprofit);
 
     }
     
--- trapingwater.java
+++ trapingwater.java
@@ -1,27 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class trapingwater {
     public static void main(String[] args) {
-        int height[] = {4,2,0,3,2,5};
+        int height[] = {4,2,0,6,3,2,5};
         int n = height.length;
-       int left_max[] = new int[n];
-       int right_max[] = new int[n];
+        int left_max[] =new int[n];
+        int right_max[] =new int[n];
 
-       left_max[0]=height[0];
-       for(int i = 1 ;i<n;i++){
-        left_max[i]=Math.max(left_max[i-1], height[i]);
+        left_max[0]=height[0];
+        for(int i = 1 ; i<n;i++){
+            left_max[i]= Math.max(left_max[i-1], height[i]);
+        }
 
-       }
+        right_max[n-1]=height[n-1];
+        for (int i = n-2; i >=0; i--) {
+            right_max[i]= Math.max(right_max[i+1], height[i]);
+            
+        }
+        int totalarea = 0;
+        System.out.println(Arrays.toString(left_max));
+        System.out.println(Arrays.toString(right_max));
 
-       right_max[n-1]=height[n-1];
-       for(int i =n-2;i>=0;i--){
-        right_max[i]=Math.max(right_max[i+1], height[i]);
-       }
 
-       int totalarea = 0;
+        for (int i = 0; i < n; i++) {
+            totalarea+= Math.min(right_max[i], left_max[i])-height[i];
+            
+            
+        }
+        System.out.println(totalarea);
 
-       for(int i = 0 ; i<n-1;i++){
-        totalarea+=(Math.min(right_max[i], left_max[i])-height[i]);
-       }
-
-       System.out.println(totalarea);
+       
     }
 }
