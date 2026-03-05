be6648cd52b89617a462b676287ebb046bb8dc70
dhruv bishnoi <bishnoishab502@gmail.com>
2026-03-05 20:35:39 +0530

    Update stringcompress.java

--- stringcompress.java
+++ stringcompress.java
@@ -1,31 +1,38 @@
 public class stringcompress {
-    
-    public static void main(String[] args) {
-        
-        StringBuilder chars = new StringBuilder("aaabbcddd");
-        StringBuilder newstring = new StringBuilder("");
-        
-        int count = 1;
-        for (int i = 0; i < chars.length(); i++) {
-            
-            if (i < chars.length() - 1 && chars.charAt(i) == chars.charAt(i + 1)) {
-                count++;
-            }
-            else{
-                if (count>1) {
-                    newstring.append(chars.charAt(i) );
-                    newstring.append(count );
-                    
-                }
-                else{
-                    newstring.append(chars.charAt(i));
 
+    public static void main(String[] args) {
+        char[] chars = { 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b' };
+        int index = 0;
+        int count = 1;
+
+        for (int i = 0; i < chars.length; i++) {
+
+            if (i < chars.length - 1 && chars[i] == chars[i + 1]) {
+                count++;
+
+            } else {
+                
+                
+                if (count > 1) {
+                    
+                    String couString = String.valueOf(count);
+                    chars[index] = chars[i];
+index++;
+                    for (char s : couString.toCharArray()) {
+                        chars[index++] = s;
+                    }
+                    
+                } else {
+                    chars[index] = chars[i];
+index++;
                 }
                 count = 1;
             }
-            
+
         }
-        System.out.println(newstring);
+        for (int i = 0; i < index; i++) {
+            System.out.print(chars[i] + " ");
+        }
     }
-    
+
 }
