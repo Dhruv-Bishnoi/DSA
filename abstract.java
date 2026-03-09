e5b421afd391da768eb4323dff1c7d5094293d55
dhruv bishnoi <bishnoishab502@gmail.com>
2026-03-09 23:13:38 +0530

    .

--- abstract.java
+++ abstract.java
@@ -1,3 +1,26 @@
-public class abstract {
-    public static void main(){}
+public class ABSTRACT{
+public static void main(String[] args){
+        dog s1 = new dog();
+    }
 }
+
+
+    abstract class Animal{
+        Animal(){
+            System.out.println("constructor is called in animal");
+        }
+
+        abstract void leg();
+    }
+     class dog extends Animal {
+
+        dog(){
+            System.out.println("constructor is called in dog");
+        }
+        void leg(){
+            System.out.println("4leg");
+        }
+
+    
+         
+    }
