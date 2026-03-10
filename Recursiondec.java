c8a2915b99654b89f6a861c374029faa5ac0b1d5
dhruv bishnoi <bishnoishab502@gmail.com>
2026-03-10 12:20:55 +0530

    Create Recursiondec.java

---
+++ Recursiondec.java
@@ -0,0 +1,17 @@
+public class Recursiondec {
+
+    public static void printdec(int n ){
+        if (n==1) {
+            System.out.println(n);
+            return ;
+            
+        }
+        System.out.println(n);
+        printdec(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printdec(n);
+    }
+}
