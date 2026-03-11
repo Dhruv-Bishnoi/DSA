e5ce96cbf98aa99064899b413d36fdde425ecb84
dhruv bishnoi <bishnoishab502@gmail.com>
2026-03-11 10:24:45 +0530

    Update RecFac.java

--- RecFac.java
+++ RecFac.java
@@ -47,11 +47,28 @@ public class RecFac {
     }
 
 
+    public static boolean issorted(int arr[],int i) {
+        
+        if (i==arr.length-1) {
+            return true;
+        }
+
+        if (arr[i]>arr[i+1]) {
+            return false;
+        }
+
+
+
+        return issorted(arr, i+1);
+    }
+
 
     
     public static void main(String[] args) {
-        int n = 40;
-        System.out.println(febonacci(n));
+        int arr[]= {1,2,3,4,5,6,9,8};
+
+        System.out.println(issorted(arr, 0));
+        
         
     }
 }
