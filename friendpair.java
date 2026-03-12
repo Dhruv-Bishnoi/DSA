075850f01391d1ab8806e6313d8bcb8716e36465
dhruv bishnoi <bishnoishab502@gmail.com>
2026-03-12 21:54:36 +0530

    Update friendpair.java

--- friendpair.java
+++ friendpair.java
@@ -1,21 +1,25 @@
 public class friendpair {
 
-    public static int pair(int n ,int ways){
+    public static int pair(int n ){
 
-        if (n==0||n==1) {
+        if (n==0||n==2) {
             return 1;
             
         }
 
 
-          ways =  pair(n-1,ways) + pair(n-2,ways);
+          int ways =  pair(n-1) + pair(n-2);
+
+
+
+          
 
          return ways;
 
 
     }
     public static void main(String[] args){
-        System.out.println(pair(3,1));
+        System.out.println(pair(4));
 
 
     }
