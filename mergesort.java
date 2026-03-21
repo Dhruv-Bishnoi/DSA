7cfb5acc2941466b3670c48a61ee46cd9430b57a
dhruv bishnoi <bishnoishab502@gmail.com>
2026-03-21 23:54:33 +0530

    Update mergesort.java

--- mergesort.java
+++ mergesort.java
@@ -3,15 +3,17 @@ public class mergesort {
 
     public static void SortUseingMerge(int arr[] , int st , int end ){
 
+        
         if (st==end) {
             return ;
             
         }
-
+        
         int mid = st+(end-st)/2;
-        System.out.println(arr[mid] + " " + mid);
+        // System.out.println(arr[mid] + " " + mid);
         SortUseingMerge(arr, st, mid);
         SortUseingMerge(arr, mid+1, end);
+
 
 
 
@@ -21,18 +23,40 @@ public class mergesort {
 
 
     public static void merge(int arr[] , int st , int end , int mid){
-        int temp[] = new int[end-st+1];
-        int i = st;
-        int j = mid+1;
-        int k = 0 ;
 
-        while (i<=mid && j <= end) {
-            if (arr[i] < arr[j]) {
-                temp[k]= arr[i];
-                
-            }
+    int temp[] = new int[(end-st)+1];
+    int i = st;
+     int j = mid+1;
+     int k =  0 ;
+
+
+     while (st<=mid && j<=end) {
+
+        if (arr[i]<arr[j]) {
+            temp[k++]=arr[i++];
+
+
             
         }
+
+        else{
+            temp[k++]=arr[j++];
+        }
+
+        while (i<=mid) {
+        temp[k++]=arr[i++];
+
+
+            
+        }
+        while (j<=mid) {
+        temp[k++]=arr[j++];            
+        }
+        
+        
+     }
+
+    
 
         
 
