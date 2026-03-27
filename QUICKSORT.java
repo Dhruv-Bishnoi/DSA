db1314f240dbe84e99a0cc0738415343ce3e9bf8
dhruv bishnoi <bishnoishab502@gmail.com>
2026-03-27 20:14:49 +0530

    Create QUICKSORT.java

---
+++ QUICKSORT.java
@@ -0,0 +1,27 @@
+public class QUICKSORT {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public static void Quicksort(int arr, int st , int end){
+
+
+
+    }
+
+    public static int partition(int []arr, int st , int end){
+
+
+
+        int povit = arr[end];
+        int i =  st-1;
+
+        for(int j=st = 0 ,)
+
+
+
+        return povit;
+
+
+    }}
