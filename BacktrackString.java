74e3dfdc7df921d613369a42176dfd2004384fce
dhruv bishnoi <bishnoishab502@gmail.com>
2026-04-03 21:51:32 +0530

    Create BacktrackString.java

---
+++ BacktrackString.java
@@ -0,0 +1,34 @@
+public class BacktrackString {
+
+
+    public static void main(String[] args) {
+
+        String str = "abc";
+        subset(str, "", 0);
+        
+    }
+
+    public static void subset(String str , String ans , int i ){
+
+        if (i==str.length()) {
+
+            if (ans.length()==0) {
+                System.out.println("null");
+                
+            }
+            else{
+
+                System.out.println(ans);
+            }
+
+            return;
+            
+        }
+
+        subset(str, ans+str.charAt(i), i+1);
+
+        subset(str, ans, i+1);
+
+    }
+    
+}
