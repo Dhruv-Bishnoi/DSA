ad37d1e8e4fd1e77b18bf28be7b39d5c8f27d194
dhruv bishnoi <bishnoishab502@gmail.com>
2026-04-06 23:23:39 +0530

    Implement Sudoku solver with backtracking
    Convert board to char[][] and add a working backtracking solver: rename issafe->isSafe and solvesudoku->solveSudoku, implement proper row/column/3x3 grid checks, try digits '1'..'9', place/backtrack on failure, and handle filled cells. Also add solution printing and success/failure messages, and reformat the board initialization.

--- sudoku.java
+++ sudoku.java
@@ -1,94 +1,95 @@
 public class sudoku {
 
     public static void main(String[] args) {
-       char[][] board = {
-    {'5','3','.','.','7','.','.','.','.'},
-    {'6','.','.','1','9','5','.','.','.'},
-    {'.','9','8','.','.','.','.','6','.'},
-    {'8','.','.','.','6','.','.','.','3'},
-    {'4','.','.','8','.','3','.','.','1'},
-    {'7','.','.','.','2','.','.','.','6'},
-    {'.','6','.','.','.','.','2','8','.'},
-    {'.','.','.','4','1','9','.','.','5'},
-    {'.','.','.','.','8','.','.','7','9'}
-};
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
 
-
-    }
-    public static boolean issafe( int board[][],int row,int colm,int digit){
-
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][colm]==digit) {
-                return false;
-                
+        if (solveSudoku(board, 0, 0)) {
+            System.out.println("solution found");
+             for(char[] row : board){
+            for(char c : row){
+                System.out.print(c + " ");
             }
-            if (board[row][colm]==digit) {
-                return false;
-                
-            }
-
-            int topgrid = (row/3)*3;
-            int sidegrid =(colm/3)*3;
-
-            for (int j = topgrid; j <topgrid+3; j++) {
-
-                for (int j2 = sidegrid; j2 <sidegrid+3; j2++) {
-
-                    if (board[j][j2]==digit) {
-                        return false;
-                        
-                    }
-                    
-                }
-                
-            }
+            System.out.println();
+        }
+            
+        }
+        else{
+            System.out.println("can not be solved");
         }
 
+        // print result
+       
+    }
+
+    public static boolean isSafe(char[][] board, int row, int col, char d){
+
+        // column
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == d) return false;
+        }
+
+        // row
+        for(int j = 0; j < 9; j++){
+            if(board[row][j] == d) return false;
+        }
+
+        // grid
+        int sr = (row/3)*3;
+        int sc = (col/3)*3;
+
+        for(int i = sr; i < sr+3; i++){
+            for(int j = sc; j < sc+3; j++){
+                if(board[i][j] == d) return false;
+            }
+        }
 
         return true;
-
     }
 
-    public static boolean solvesudoku(int board[][], int row , int colm ){
+    public static boolean solveSudoku(char[][] board, int row , int col){
 
-
-        if (colm==9) {
-
-            colm=0;
+        if(col == 9){
             row++;
-            
+            col = 0;
         }
+
         if(row == 9) return true;
 
-    // skip filled cell
-      if(board[row][colm] != '.'){
-        return solvesudoku(board, row, colm+1);
-    }
+        if(board[row][col] != '.'){
+            if(isSafe(board, row, col, board[row][col])){
+                
+                return solveSudoku(board, row, col+1);
+            }
+            else{
 
-        for (int i = 1; i <=9; i++) {
-            
-            if (issafe(board,row,colm,i)) {
+                return false;
+            }
+        }
 
-                if(solvesudoku(board, row, colm)){
+        for(char d = '1'; d <= '9'; d++){
+
+            if(isSafe(board, row, col, d)){
+
+                board[row][col] = d;
+
+                if(solveSudoku(board, row, col+1)){
                     return true;
                 }
-                 
 
-    
-                
+                board[row][col] = '.'; // backtrack
             }
-            else if(i==9 && !issafe(board,row,colm,i)){
-                return false ;
-            }
-
-            solvesudoku(board, row, colm);
-
-
         }
+
         return false;
-
-
-
-        
     }
 }
