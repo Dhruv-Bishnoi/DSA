ecb01130999f1e9bbb060679cbe0170ef3d3f3d2
dhruv bishnoi <bishnoishab502@gmail.com>
2026-04-12 21:50:20 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,7 @@
 public class LinkedList {
     public static Node Head;
     public static Node Tail;
+    public static int size;
 
     public static class  Node {
         int data;
@@ -15,46 +16,183 @@ public class LinkedList {
         }
 
 
-        public void Firstadd(int data){
+        
+    }
+    public void Firstadd(int data){
 
 
-            Node newNode = new Node(data);
+        Node newNode = new Node(data);
+        size++;
 
-            if (Head==null) {
-                Head = Tail = newNode;
-                return;
+        if (Head==null) {
+            Head = Tail = newNode;
+            return;
 
-                
-            }
-            newNode.next = Head;
+            
+        }
+        newNode.next = Head;
 
-            Head = newNode;
+        Head = newNode;
+    }
+
+
+    public void Lastadd(int data){
+
+
+
+        Node newNode = new Node(data);
+                size++;
+
+         if (Head==null) {
+            Head = Tail = newNode;
+            return;
+
+            
+        }
+
+        Tail.next = newNode;
+        Tail = newNode;
+
+    }
+
+
+    public void printNode(){
+         if (Head == null) {
+            System.out.println("empty");
+            return ;
+         }
+        Node temp = Head ; 
+
+        while(temp != null) {
+
+            System.err.print(temp.data + " ");
+            temp = temp.next;
+
+            
+        } 
+        System.err.println();  
+    }
+
+    public void add(int index , int data){
+        if (index == 0 ) {
+            Firstadd(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+                size++;
+ 
+        int i = 0 ;
+
+        Node temp = Head;
+
+        while (i<index-1) {
+            temp = temp.next;
+            i++;
+            
+        }
+
+        newNode.next = temp.next;
+        temp.next=newNode;
+    }
+    
+    public int removeFirst(){
+        if (size == 0) {
+
+            return Integer.MIN_VALUE;
+            
+        }
+        else if (size == 1) {
+            int val =  Head.data;
+            Tail = Head = null;
+            size--;
+            return val;
+        }
+
+        int val = Head.data;
+        
+        Head = Head.next;
+        size--;
+
+        return val;
+
+    }
+    public int removeLast(){
+        if (size == 0) {
+
+            return Integer.MIN_VALUE;
+            
+        }
+        else if (size == 1) {
+            int val =  Head.data;
+            Tail = Head = null;
+            return val;
+        }
+
+        Node temp = Head;
+        int i = 0;
+
+        while (i < size-2) {
+            temp = temp.next;
+            i++;
+
+        }
+        int val = temp.data;
+        temp.next = null;
+        Tail = temp;
+        size--;
+        return val;
+
+
+    }
+
+    public int search(int key ){
+
+        Node temp = Head;
+
+        if (temp == null) {
+
+            return 0;
+            
         }
 
 
-        public void Lastadd(int data){
-
-
-
-            Node newNode = new Node(data);
-             if (Head==null) {
-                Head = Tail = newNode;
-                return;
-
+            if (temp.data == key) {
+            
+                return 1;
                 
             }
 
-            Tail.next = newNode;
-            Tail = newNode;
 
-        }
+
+            temp = temp.next;
+            int in = search(key);
+            in++;
+            return in;
 
 
         
+
     }
     public static void main(String[] args) {
 
         LinkedList ll = new LinkedList();
-        
+        ll.Firstadd(34); 
+        ll.Firstadd(5);       
+        ll.Lastadd(54);
+        ll.printNode();
+        ll.add(1, 65);
+        ll.printNode();
+        ll.removeFirst();
+        ll.printNode();
+        ll.removeLast();
+        ll.printNode();
+        System.out.println(ll.search(5));
+        System.out.println(ll.search(65));
+        ll.search(65);
+
+
+
+
     }
 }
