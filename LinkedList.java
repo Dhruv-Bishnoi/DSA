dbcdfa190104e7fa762c78dff899ca6237e2f888
dhruv bishnoi <bishnoishab502@gmail.com>
2026-04-13 15:27:42 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -3,51 +3,43 @@ public class LinkedList {
     public static Node Tail;
     public static int size;
 
-    public static class  Node {
+    public static class Node {
         int data;
         Node next;
 
-
-        public Node(int data){
+        public Node(int data) {
 
             this.data = data;
             this.next = null;
 
         }
 
-
-        
     }
-    public void Firstadd(int data){
 
+    public void Firstadd(int data) {
 
         Node newNode = new Node(data);
         size++;
 
-        if (Head==null) {
+        if (Head == null) {
             Head = Tail = newNode;
             return;
 
-            
         }
         newNode.next = Head;
 
         Head = newNode;
     }
 
-
-    public void Lastadd(int data){
-
-
+    public void Lastadd(int data) {
 
         Node newNode = new Node(data);
-                size++;
+        size++;
 
-         if (Head==null) {
+        if (Head == null) {
             Head = Tail = newNode;
             return;
 
-            
         }
 
         Tail.next = newNode;
@@ -55,76 +47,73 @@ public class LinkedList {
 
     }
 
-
-    public void printNode(){
-         if (Head == null) {
+    public void printNode() {
+        if (Head == null) {
             System.out.println("empty");
-            return ;
-         }
-        Node temp = Head ; 
+            return;
+        }
+        Node temp = Head;
 
-        while(temp != null) {
+        while (temp != null) {
 
             System.err.print(temp.data + " ");
             temp = temp.next;
 
-            
-        } 
-        System.err.println();  
+        }
+        System.err.println();
     }
 
-    public void add(int index , int data){
-        if (index == 0 ) {
+    public void add(int index, int data) {
+        if (index == 0) {
             Firstadd(data);
             return;
         }
 
         Node newNode = new Node(data);
-                size++;
- 
-        int i = 0 ;
+        size++;
+
+        int i = 0;
 
         Node temp = Head;
 
-        while (i<index-1) {
+        while (i < index - 1) {
             temp = temp.next;
             i++;
-            
+
         }
 
         newNode.next = temp.next;
-        temp.next=newNode;
+        temp.next = newNode;
     }
-    
-    public int removeFirst(){
+
+    public int removeFirst() {
         if (size == 0) {
 
             return Integer.MIN_VALUE;
-            
-        }
-        else if (size == 1) {
-            int val =  Head.data;
+
+        } else if (size == 1) {
+            int val = Head.data;
             Tail = Head = null;
             size--;
             return val;
         }
 
         int val = Head.data;
-        
+
         Head = Head.next;
         size--;
 
         return val;
 
     }
-    public int removeLast(){
+
+    public int removeLast() {
         if (size == 0) {
 
             return Integer.MIN_VALUE;
-            
-        }
-        else if (size == 1) {
-            int val =  Head.data;
+
+        } else if (size == 1) {
+            int val = Head.data;
             Tail = Head = null;
             return val;
         }
@@ -132,7 +121,7 @@ public class LinkedList {
         Node temp = Head;
         int i = 0;
 
-        while (i < size-2) {
+        while (i < size - 2) {
             temp = temp.next;
             i++;
 
@@ -143,22 +132,20 @@ public class LinkedList {
         size--;
         return val;
 
-
     }
 
-
-    public int  helper( Node Head , int key){
+    public int helper(Node Head, int key) {
 
         if (Head == null) {
 
             return -1;
-            
+
         }
 
         if (Head.data == key) {
 
-            return 0 ;
-            
+            return 0;
+
         }
 
         int idx = helper(Head.next, key);
@@ -166,18 +153,18 @@ public class LinkedList {
         if (idx == -1) {
 
             return -1;
-            
+
         }
 
-        return idx+1;
+        return idx + 1;
     }
 
-    public int searchbyrecursion(int key){
+    public int searchbyrecursion(int key) {
 
         return helper(Head, key);
     }
-    
-    public void search(int key){
+
+    public void search(int key) {
 
         Node temp = Head;
         int i = 0;
@@ -186,34 +173,105 @@ public class LinkedList {
 
             if (temp.data == key) {
                 System.out.println(i);
-                return ;
-                
+                return;
+
             }
 
             temp = temp.next;
             i++;
-            
+
         }
 
         System.out.println("not found");
     }
+
+    public void reverse() {
+
+        Node prev = null;
+        Node curr = Tail = Head;
+        Node Next;
+
+        while (curr != null) {
+
+            Next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = Next;
+
+        }
+
+        Head = prev;
+
+    }
+
+    public void removeNTHfromEnd(int key) {
+        int idx = size - key;
+        Node prev = Head;
+        int i = 1;
+
+        while (i < idx) {
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+        size--;
+
+    }
+
+   
+    public void palindrome() {
+        int half = 0;
+        Node prev = Head;
+        Node curr = prev.next;
+        Node Next = curr.next;
+        if (size % 2 != 0) {
+            half = (size / 2) + 1;
+        }
+        int j = 1;
+        while (j <= half) {
+            prev = curr;
+            curr = Next;
+            Next = curr.next;
+            j++;
+        }
+         while(curr != null){
+        Node next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+        Node firstHalf = Head;
+        Node secHalf = prev;
+        for (int i = 1; i < size / 2; i++) {
+            secHalf = secHalf.next;
+        }
+        if (size % 2 != 0) {
+            secHalf = secHalf.next;
+        }
+        for (int i = 1; i < size; i++) {
+            if (firstHalf.data != secHalf.data) {
+                System.out.println("not a palindrome");
+                return;
+            }
+        }
+        System.out.println("this is palimdrome");
+    }
+
     public static void main(String[] args) {
 
         LinkedList ll = new LinkedList();
-        ll.Firstadd(34); 
-        ll.Firstadd(5);       
+        ll.Firstadd(34);
+        ll.Firstadd(5);
         ll.Lastadd(54);
+        ll.Lastadd(4);
+       
         ll.printNode();
-        ll.add(1, 65);
-        ll.printNode();
-        ll.removeFirst();
-        ll.printNode();
-        ll.removeLast();
-        ll.printNode();
-        System.out.println(ll.searchbyrecursion(34));
 
-
-
+        ll.reverse();
+        ll.printNode();
+        ll.printNode();
+        ll.palindrome();
 
     }
 }
